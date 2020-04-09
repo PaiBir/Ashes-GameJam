20106a0909c1ead5ec33cb20d81ae90f2b5d5bde
Viacheslav <vi.gabitov@gmail.com>
2020-04-08 20:49:04 -0400

    Add TestScene1 scene with terrain, building and characters cubes.

---
+++ Ashes/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public float horizontalInput;
+    public float verticalInput;
+    private float speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");     
+        
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);        
+    }
+}
